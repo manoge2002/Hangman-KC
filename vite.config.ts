53e4b8f6698b7e2b7e87478195c5df1bad08d646
manoge2002 <87379386+manoge2002@users.noreply.github.com>
2026-01-20 21:41:49 +0000

    clear

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Ensures assets are loaded correctly on GH Pages subpaths
-});
